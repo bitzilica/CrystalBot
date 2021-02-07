628b09e102a61fd90b6591e2d20a3d34a51615c9
bitzilica <mihaimih361@yahoo.com>
2021-02-07 12:25:35 +0200

    CrystalBot

---
+++ main.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = ',';
+
+client.once('ready', () => {
+    console.log('Crystal Bot este online!')
+});    
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLocaleLowerCase()
+
+    if(command === 'sal'){
+        message.channel.send('Salut!');
+    }else if (command == 'salut'){
+       message.channel.send('Salut ce faci?')}
+    }    
+);
+
+client.login(process.env.token);
